486266029f1cdb425f9750f921751b565e627129
moksh-C <mokshchhetri75.com>
2025-01-14 10:56:40 -0500

    use randint and if elif and else codition makers

--- labs/Week02/lab2.py
+++ labs/Week02/lab2.py
@@ -14,7 +14,22 @@ print ["Elements:",elements]
 def get_valid_int_input(prompt):
     while True:
         try:
-            return int(input(print))
+            return int(input(prompt))
         except ValueError:
-            print("Please enter an integere!")
+            print("Error: Please enter a valid integere!")
             continue
+try:
+    elements_selected = get_valid_int_input("Enter the index of the element you like")
+    # Roll dice
+    elementRoll = random.randint(1, 6)
+    totalNum = elements_selected + elementRoll
+
+    # Print the result based on the totalNum
+    if elementRoll <= 2:
+        print("You rolled a weak element, friend.")
+    elif elementRoll <= 4:
+        print("Yor element is moderate.")
+    else:
+        print("Nice element.")
+except Exception as e:
+    print("")
