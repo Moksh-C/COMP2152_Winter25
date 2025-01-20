628b8a67224125df14fdd41542f5b98e62152296
moksh-C <mokshchhetri75.com>
2025-01-19 23:50:13 -0500

    Add lab1.py with solutions

---
+++ Week01/lab1.py
@@ -0,0 +1,19 @@
+# Sample Coding Questions 01 Week 01
+#Moksh Chhetri
+
+# Defining an array variable with elements 1, 4, 7, 9
+array_variable = [1, 4, 7, 9]
+
+
+a, b, c, d = 1, 2, 3, 4
+e1 = (a * c) - (b / d)
+e2 = (a - ((b ** c) // d)) + (a % c)
+
+
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+
+userAge = int(input("Enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
